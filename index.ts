9c9dd9bc9c0dbc0b11d9343fe39aa88dc3132736
PrakharSinghOnGit <singhprakhar1209@gmail.com>
2023-10-22 16:47:29 +0530

    Made Preloading of the miner work

--- index.ts
+++ index.ts
@@ -2,25 +2,108 @@
 console.time("Time Taken ");
 
 // importing modules
-const PATH = require("path");
-const COLORS = require("colors");
-const FILE_SYSTEM = require("fs");
-const { log } = require("console");
-const { MultiSelect } = require("enquirer");
-const CLI_WIDTH = require("cli-width");
-const POWER = require("child_process");
-const PUPPETEER = require("puppeteer-extra");
-const axios = require("axios");
-const csvtojson = require("csvtojson");
-const { PendingXHR } = require("pending-xhr-puppeteer");
-const STEALTH_PLUGIN = require("puppeteer-extra-plugin-stealth");
-const { Cluster } = require("puppeteer-cluster");
+import ora from "ora";
+import PATH from "path";
+import axios from "axios";
+import COLORS from "colors";
+import FILE_SYSTEM from "fs";
+import { log } from "console";
+import csvtojson from "csvtojson";
+import CLI_WIDTH from "cli-width";
+import POWER from "child_process";
+
+// requiring modules
+const Bun = require("bun");
+const { MultiSelect, Confirm } = require("enquirer");
 
 // assigning constants
-const Setting = JSON.parse(FILE_SYSTEM.readFileSync("./Settings.json"));
-const TERMINATOR = "-=".repeat(CLI_WIDTH() / 4);
+const Setting = JSON.parse(await Bun.file("./Settings.json").text());
+const TERMINATOR = () => log(COLORS.dim("=-".repeat(CLI_WIDTH() / 4)));
 const LEVELS = Setting.Miner.Levels;
-const SLEEP = (duration) =>
-  new Promise((resolve) => setTimeout(resolve, duration));
-PUPPETEER.use(STEALTH_PLUGIN());
-let wrong = "";
+
+// Preload
+console.clear();
+TERMINATOR();
+
+// Output File Handling
+if (!FILE_SYSTEM.existsSync(PATH.join(__dirname, "/out")))
+  FILE_SYSTEM.mkdirSync(PATH.join(__dirname, "/out"));
+var out = FILE_SYSTEM.readdirSync(PATH.join(__dirname, "/out")).filter((el) =>
+  el != "json" ? el : ""
+);
+try {
+  out.forEach((el) => {
+    FILE_SYSTEM.unlinkSync(PATH.join(__dirname, "/out", el));
+  });
+} catch (error) {
+  log(COLORS.red("ERROR : Please Close File to continue !"));
+  process.exit(0);
+}
+
+//getting names from keys
+const TeamsOPT = Object.keys(Setting.Links).filter(
+  ([key]) => !key.startsWith("#")
+);
+
+// Asking for which team to use
+const Func = await new MultiSelect({
+  name: "Func",
+  initial: "LEVEL DATA",
+  message: COLORS.yellow("Select Function ?"),
+  choices: ["LEVEL DATA", "TARGET DATA", "CHEQUE DATA"],
+}).run();
+
+if (Func.length === 0) {
+  log(`${COLORS.red("-ERROR- :")} ${COLORS.yellow("Please Select Function")}`);
+  process.exit(0);
+}
+
+// // asking for which team to use
+const Teams = await new MultiSelect({
+  name: "Teams",
+  initial: TeamsOPT[0],
+  message: COLORS.yellow("Select Teams ?"),
+  choices: TeamsOPT,
+}).run();
+
+if (Teams.length == 0) {
+  log(`${COLORS.red("-ERROR- :")} ${COLORS.yellow("Please Select Team")}`);
+  process.exit(0);
+}
+
+for (let i = 0; i < Teams.length; i++) {
+  // loop for every team
+  const spinner = ora(
+    COLORS.magenta("Fetching Data for : " + Teams[i])
+  ).start();
+  const url = Setting.Links[Teams[i]];
+  const csv = await axios.get(url);
+  const Data = await csvtojson().fromString(csv.data);
+  spinner.succeed(COLORS.green("Fetched Data for : " + Teams[i]));
+  TERMINATOR();
+  log(
+    COLORS.magenta("FILE NBR :"),
+    COLORS.yellow((i + 1).toString()) + "/" + COLORS.yellow(Teams.length)
+  );
+  log(
+    COLORS.magenta(Teams[i].toString().toUpperCase()) +
+      ":" +
+      COLORS.yellow(Data.length.toString())
+  );
+  TERMINATOR();
+  // await MINER(data, Func, Teams[i]); // actual Data Fetch
+}
+TERMINATOR();
+log(COLORS.green.bold("PROCESS COMPLETED"));
+const prompt = await new Confirm({
+  name: "question",
+  message: COLORS.yellow("OPEN OUTPUT DIRECTORY?"),
+  initial: false,
+}).run();
+if (prompt) {
+  POWER.execSync("open " + PATH.join(__dirname, "/out"));
+  log("Opened", COLORS.magenta(PATH.join(__dirname, "/out")));
+}
+TERMINATOR();
+
+export {};
---
+++ miner.ts
@@ -0,0 +1,121 @@
+// importing Modules
+import { PendingXHR } from "pending-xhr-puppeteer";
+import { Cluster } from "puppeteer-cluster";
+import PUPPETEER from "puppeteer-extra";
+import STEALTH_PLUGIN from "puppeteer-extra-plugin-stealth";
+import CHROMIUM from "chromium";
+import COLORS from "colors";
+import { log } from "console";
+
+// Requiring Modules
+const Bun = require("bun");
+
+// Assigning Constants
+PUPPETEER.use(STEALTH_PLUGIN());
+const Setting = JSON.parse(await Bun.file("./Settings.json").text());
+
+async function MINER(DATA, FUNCTION, FILENAME) {
+  const cluster = await Cluster.launch({
+    // browser Launch Properties
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    workerCreationDelay: 10,
+    monitor: Setting.Miner.Debug ? false : false,
+    timeout: 60 * 1000,
+    retryLimit: 0,
+    retryDelay: 10,
+    maxConcurrency: Setting.Miner.Debug ? 1 : Setting.Miner.MaxConcurrency,
+    puppeteer: PUPPETEER,
+    puppeteerOptions: {
+      headless: Setting.Miner.Debug ? false : false,
+      slowMo: Setting.Miner.Debug ? 0 : Setting.Miner.SlowMo,
+      defaultViewport: null,
+      args: [
+        `--start-maximized`,
+        `--headless=new`,
+        `--auto-open-devtools-for-tabs`,
+        `--incognito`,
+      ],
+    },
+  });
+  cluster.on("taskerror", async (err, { name }) => {
+    // Error Handling
+    log(
+      `${COLORS.red("-ERROR- :")} ${COLORS.yellow(name)} : ${COLORS.dim(
+        err.message.toString()
+      )}`
+    );
+  });
+  const LEVEL_OUT = [];
+  const TARGET_OUT = [];
+  const CHEQUE_OUT = [];
+  await cluster.task(async ({ page, data: { id, pass, name } }) => {
+    // Fetching Data
+    await page.setRequestInterception(true); // Not Loading FONT and IMAGE
+    page.on("request", (req) => {
+      if (req.resourceType() == "font" || req.resourceType() == "image")
+        req.abort();
+      else req.continue();
+    });
+    // ---------------------- Calling Awpl Functions Accordingly ----------------------
+    // await LOGIN(page, id, pass, name);
+    // if (FUNCTION.includes("LEVEL DATA")) {
+    //   var data = await LEVEL(page, name);
+    //   funcName = "LEVEL";
+    //   log(
+    //     `${COLORS.green("SUCCESS")} : ${COLORS.blue(
+    //       funcName
+    //     )}  : ${COLORS.yellow(name)} : ${COLORS.dim(data.level)}`
+    //   );
+    //   LEVEL_OUT.push(data); // push data to output
+    // }
+    // if (FUNCTION.includes("TARGET DATA")) {
+    //   var data = await TARGET(page, name);
+    //   funcName = "TARGET";
+    //   log(
+    //     `${COLORS.green("SUCCESS")} : ${COLORS.blue(
+    //       funcName
+    //     )} : ${COLORS.yellow(name)} : ${COLORS.dim(data.level)}`
+    //   );
+    //   TARGET_OUT.push(data); // push data to output
+    // }
+    // if (FUNCTION.includes("CHEQUE DATA")) {
+    //   var data = await CHEQUE(page, name);
+    //   const totalAmount = data.data.reduce((total, item) => {
+    //     const amount = parseFloat(item.amount); // Parse the amount as a float
+    //     return total + amount;
+    //   }, 0);
+    //   funcName = "CHEQUE";
+    //   log(
+    //     `${COLORS.green("SUCCESS")} : ${COLORS.blue(
+    //       funcName
+    //     )} : ${COLORS.yellow(name)} : ${COLORS.dim(data.level)} : ${COLORS.dim(
+    //       totalAmount
+    //     )}`
+    //   );
+    //   CHEQUE_OUT.push(data); // push data to output
+    // }
+  });
+  for (let i = 0; i < DATA.length; i++) {
+    // calling Fetch for every Member
+    cluster.queue(DATA[i]);
+  }
+  await cluster.idle(); // closing when done
+  await cluster.close(); // closing when done
+  if (LEVEL_OUT.length != 0) {
+    // Handle Level Output File
+    // const Sorted_Level_Out = await SORT(LEVEL_OUT, true, "Level"); // sorting Out Data by Level
+    // await PRINT(Sorted_Level_Out, FILENAME + " Level Data");
+  }
+  if (TARGET_OUT.length != 0) {
+    // Handle Target Output File
+    // const Sorted_Target_Out = await SORT(TARGET_OUT, true, "Target"); // sorting Out Data by Level
+    // await PRINT(Sorted_Target_Out, FILENAME + " Target Data");
+  }
+  if (CHEQUE_OUT.length != 0) {
+    // Handle Cheque Output FIle
+    // const Sorted_Cheque_Out = await SORT(CHEQUE_OUT);
+    // await PRINT(Sorted_Cheque_Out, FILENAME + " Cheque Data", "Cheque");
+  }
+}
+
+export default MINER;
